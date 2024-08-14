294a82cce05540b1c9c802a105a4f75a56e1b8dd
Guilherme <guilherme.benedito@fatec.sp.gov.br>
2024-08-14 12:21:42 -0300

    aula

---
+++ index.js
@@ -0,0 +1,10 @@
+let boasVindas = (nome) => {
+    return `Bem-vindo aos módulos, ${nome}`
+}
+
+let opniao = (oQueVoceAcha) => {
+    return `Agora eu acho que usar módulos é ${oQueVoceAcha}`
+}
+
+export default boasVindas
+export {opniao}
